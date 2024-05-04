c8f81249b3582f40b819655b71c4657fcf0e51bc
mudasir21 <mudasirkhan09413@example.com>
2024-05-04 13:16:15 +0530

    creating navbar and dark-light mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,13 @@ import Projects from './pages/Projects'
 import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'
 import About from './pages/About'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
+
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
